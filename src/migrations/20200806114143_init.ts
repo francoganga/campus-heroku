7af622ebc9c083794a26206e0d20d66fe73930db
fganga <fganga@unaj.edu.ar>
2020-08-06 18:05:40 -0300

    added uuid extension on migration.

--- src/migrations/20200806114143_init.ts
+++ src/migrations/20200806114143_init.ts
@@ -1,10 +1,16 @@
 import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
-  return knex.schema.createTable('graphs', (table) => {
-    table.uuid('graph_id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
-    table.string('description');
-  });
+  return knex.schema
+    .raw('CREATE EXTENSION IF NOT EXISTS "uuid-ossp"')
+    .createTable('graphs', (table) => {
+      table
+        .uuid('graph_id')
+        .primary()
+        .notNullable()
+        .defaultTo(knex.raw('uuid_generate_v4()'));
+      table.string('description');
+    });
 }
 
 export async function down(knex: Knex): Promise<void> {
