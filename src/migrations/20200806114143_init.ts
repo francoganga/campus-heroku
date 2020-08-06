3d1d8ec1cf8152f7c796a0e056fc732ce59c818a
fganga <fganga@unaj.edu.ar>
2020-08-06 15:40:55 -0300

    Fix: uuid auto generate on item creation.

--- src/migrations/20200806114143_init.ts
+++ src/migrations/20200806114143_init.ts
@@ -2,7 +2,7 @@ import * as Knex from 'knex';
 
 export async function up(knex: Knex): Promise<void> {
   return knex.schema.createTable('graphs', (table) => {
-    table.uuid('graph_id').primary();
+    table.uuid('graph_id').primary().defaultTo(knex.raw('uuid_generate_v4()'));
     table.string('description');
   });
 }
