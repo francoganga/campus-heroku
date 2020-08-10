a3bbcd7be2cde1e96d4d3d6061133f57bb690ab6
fganga <fganga@unaj.edu.ar>
2020-08-10 19:46:15 -0300

    adding cors.

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,18 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import api from './routes/api';
+import cors from 'cors';
 
 dotenv.config({ path: '.env' });
 
 const app = express();
 
 app.use(express.json());
+app.use(
+  cors({
+    origin: 'http://localhost:3000',
+  })
+);
 
 const PORT = process.env.PORT || 8000;
 
